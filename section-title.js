2549100f27d92e22054983c2d0fcbc997ddd9fe7
erikna7 <erikson.saragi@humantechno.co.id>
2024-08-02 18:20:31 +0700

    add progress circle

--- section-title.js
+++ section-title.js
@@ -1,22 +1,68 @@
 window.onload = () => {
-  let sectionTitle = document.getElementById('active-section');
+  let sectionTitle = document.getElementById('section-title');
   const sections = document.querySelectorAll('section');
+  let percentage = document.getElementById('percentage');
+  let progress = 0;
+  let myChart = null;
+  
+  const createChart = (completed, remaining) => {
+    const ctx = document.getElementById('myChart').getContext('2d');
 
-  console.log(sections.length);
+    if (myChart !== null) {
+      myChart.destroy();
+    }
+
+    myChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        datasets: [{
+          label: '',    
+          data: [completed, remaining],
+          borderWidth: 0,
+          backgroundColor: [
+            'rgb(255, 255, 255)',
+            'rgb(157, 157, 157)',
+          ],
+        }]
+      },
+      options: {
+        cutout: 22,
+        tooltips: {enabled: false},
+        hover: {mode: null},
+      }
+    });
+  }
+
+  const updateChart = (chart, progress, remaining) => {
+    const prev = chart.data.datasets[0].data
+    console.log('prev cart data', prev)
+    if (prev[0][0] === sections.length) return;
+    
+    chart.data.datasets[0].data = [progress, remaining];
+    chart.update();
+  }
+
   function getActiveSection() {
-    let visibleSection = null
+    let visibleSection = null;
     sections.forEach((section, index) => {
       const rects = section.getBoundingClientRect();
-      if (rects.top < 200 && rects.bottom > 200) {
+      if (rects.top < 250 && rects.bottom > 250) {
         visibleSection = section;
+        if (progress !== sections.length) {
+          progress = index + 1;
+        }
       }
     });
 
     if (visibleSection) {
       sectionTitle.innerHTML = visibleSection.firstElementChild.textContent;
+      percentage.innerHTML = Math.round((progress / sections.length) * 100) + '%';
     }
+    updateChart(myChart, progress, sections.length - progress);
   }
 
   window.addEventListener('scroll', getActiveSection);
+  
+  createChart(0, sections.length)
   getActiveSection();
 }
